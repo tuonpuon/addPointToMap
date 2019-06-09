98d63b458c231fc48b9416578e2247df2558bbdb
patrik <tuon89@gmail.com>
2019-06-09 08:48:46 +0200

    added vue client to frontend

---
+++ Frontend/client/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app" class="vh-100">
+    <!-- navbar: start -->
+    <nav class="navbar navbar-light bg-light position-sticky">
+      <a class="navbar-brand" href="#">Navbar</a>
+    </nav>
+    <!-- navbar: end -->
+    <router-view />
+  </div>
+</template>
---
+++ Frontend/client/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container-fluid custom-height">
+    <div class="row h-100">
+      <Tickets msg="test"></Tickets>
+      <Map msg="test2"></Map>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import Map from "@/components/Map.vue";
+import Tickets from "@/components/Tickets.vue";
+
+export default {
+  name: "home",
+  components: {
+    Map,
+    Tickets
+  }
+};
+</script>
+
+<style scoped>
+  .custom-height {
+      height: 90% !important;
+  }
+</style>
+
